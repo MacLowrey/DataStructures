9c21840ad935531ca8de4ff4b3e830b130b9d243
Jon Lowrey <Jon@minerva.local>
2015-10-25 12:15:31 -0500

    Initial Commit

---
+++ timeMeasurement/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  timeMeasurement
+//
+//  Created by Jon Lowrey on 10/25/15.
+//  Copyright © 2015 Jon Lowrey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
